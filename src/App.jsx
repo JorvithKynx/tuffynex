095747a5cfbe7ee495264e248234aff1bbaac0d2
Abdulkadir Ceyhan <jorvithkynx@gmail.com>
2024-11-20 13:35:06 +0300

    Automated commit

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,14 @@ import ForgotPassword from "./pages/ForgotPassword";
 
 const App = () => {
 	return (
-		<Router> <Routes> <Route path="/" element={
-			<Home/>}/> <Route path="/auth/login" element={
-			<Login/>}/> <Route path="/auth/register" element={
-			<Register/>}/> <Route path="/auth/forgot-password" element={
-			<ForgotPassword/>}/> </Routes> </Router>
+		<Router>
+			<Routes>
+				<Route path="/" element={<Home/>}/>
+				<Route path="/auth/login" element={<Login/>}/>
+				<Route path="/auth/register" element={<Register/>}/>
+				<Route path="/auth/forgot-password" element={<ForgotPassword/>}/>
+			</Routes>
+		</Router>
 	);
 };
 
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,17 +1,104 @@
-import React from 'react';
+import React, {useState} from 'react';
+
+import {Link} from 'react-router-dom';
+
+import '../styles/Register.scss';
 
 const Register = () => {
-	return (
-		<div>
-			<h1>Kayıt Sayfası</h1>
-			<form>
-				<label>Ad:</label> <input type="text" placeholder="Adınız"/> <br/> <label>Email:</label>
-				<input type="email" placeholder="Email"/> <br/> <label>Şifre:</label>
-				<input type="password" placeholder="Şifre"/> <br/>
-				<button type="submit">Kayıt Ol</button>
-			</form>
+	
+	const appName = import.meta.env.VITE_STR_APPNAME;
+	
+	const [username, setUsername] = useState('');
+	const [password, setPassword] = useState('');
+	const [accepted, setAccepted] = useState(false);
+	
+	const registerHandler = (_username, _password, _accepted) => {
+		
+		_username = username;
+		_password = password;
+		_accepted = accepted;
+		
+		
+		console.log('Login Handler:', _username, _password, _accepted);
+		
+	}
+	
+	return (<div className="body-wrapper">
+		<div className="auth-body">
+			<div className="auth-content">
+				<div className="auth-box">
+					<div className="container">
+						<div className="row">
+							
+							<div className="col-md-6 d-none d-sm-none d-md-block auth-col-left">
+								<img src="/assets/img/auth-forms-left.png" alt="Logo" className="img-fluid"/>
+							</div>
+							
+							<div className="col-md-6 col-sm-12 auth-col-right">
+								
+								<div className="auth-form">
+									
+									<h6>Start for Free</h6>
+									<h4>Sign up to {appName}</h4>
+									
+									<div className="mb-3">
+										<label htmlFor="register-username" className="form-label">Kullanıcı Adı</label>
+										<div className="input-group">
+											<i className="fa-sharp fa-thin fa-at"></i>
+											<input type="text" className="form-control" id="register-username"
+												value={username}
+												onChange={(e) => setUsername(e.target.value)}
+											/>
+										</div>
+									</div>
+									
+									<div className="mb-3">
+										<label htmlFor="register-password" className="form-label">Şifre</label>
+										<div className="input-group">
+											<i className="fa-sharp fa-thin fa-lock"></i>
+											<input type="password" className="form-control" id="register-password" placeholder="******"
+												value={password}
+												onChange={(e) => setPassword(e.target.value)}
+											/>
+										</div>
+									</div>
+									
+									<div className="d-flex justify-content-between mb-3">
+										
+										<div className="form-check">
+											<input type="checkbox" className="form-check-input" id="register-accepted"
+												checked={accepted}
+												onChange={(e) => setAccepted(e.target.checked)}
+											/>
+											<label htmlFor="register-accepted" className="form-check-label">Sözleşmeyi Okudum & Onaylıyorum.</label>
+										</div>
+									
+									</div>
+									
+									<div className="d-grid mb-3">
+										<button type="button" className="btn btn-theme" onClick={registerHandler}>
+											<span>Continue</span>
+											<i className="fa-sharp fa-thin fa-angle-double-right"></i>
+										</button>
+									</div>
+									
+									<div className="mb-3">
+										<p className="linkToRegister">
+											<span>Zaten Üye Misiniz ? </span>
+											<Link to="/auth/login">Şimdi Giriş Yapın</Link>
+										</p>
+									</div>
+								
+								</div>
+							
+							</div>
+						
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
-	);
+	</div>);
 };
 
 export default Register;
