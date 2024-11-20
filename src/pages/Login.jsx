eef2fdb635b4e17767c4dc10673d3c7120807892
Abdulkadir Ceyhan <jorvithkynx@gmail.com>
2024-11-20 08:17:29 +0300

    Automated commit

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,6 @@
-import React, {
-	useState
-} from 'react';
+import React, {useState} from 'react';
 
-import {
-	Link
-} from 'react-router-dom';
+import {Link} from 'react-router-dom';
 
 import '../styles/Login.scss';
 
@@ -27,82 +23,80 @@ const Login = () => {
 		
 	}
 	
-	return (
-		<div className="body-wrapper">
-			<div className="auth-body">
-				<div className="auth-content">
-					<div className="auth-box">
-						<div className="container">
-							<div className="row">
+	return (<div className="body-wrapper">
+		<div className="auth-body">
+			<div className="auth-content">
+				<div className="auth-box">
+					<div className="container">
+						<div className="row">
+							
+							<div className="col-md-6 d-none d-sm-none d-md-block auth-col-left">
+								<img src="/assets/img/auth-forms-left.png" alt="Logo" className="img-fluid"/>
+							</div>
+							
+							<div className="col-md-6 col-sm-12 auth-col-right">
 								
-								<div className="col-md-6 d-none d-sm-none d-md-block auth-col-left">
-									<img src="/assets/img/auth-forms-left.png" alt="Logo" className="img-fluid"/>
-								</div>
-								
-								<div className="col-md-6 col-sm-12 auth-col-right">
+								<div className="auth-form">
 									
-									<div className="auth-form">
+									<h6>Start for Free</h6>
+									<h4>Sign in to {appName}</h4>
+									
+									<div className="mb-3">
+										<label htmlFor="login-username" className="form-label">Kullanıcı Adı</label>
+										<div className="input-group">
+											<i className="fa-sharp fa-thin fa-at"></i>
+											<input type="text" className="form-control" id="login-username"
+												value={username}
+												onChange={(e) => setUsername(e.target.value)}
+											/>
+										</div>
+									</div>
+									
+									<div className="mb-3">
+										<label htmlFor="login-password" className="form-label">Şifre</label>
+										<div className="input-group">
+											<i className="fa-sharp fa-thin fa-lock"></i>
+											<input type="password" className="form-control" id="login-password" placeholder="******"
+												value={password}
+												onChange={(e) => setPassword(e.target.value)}
+											/>
+										</div>
+									</div>
+									
+									<div className="d-flex justify-content-between mb-3">
 										
-										<h6>Start for Free</h6>
-										<h4>Sign in to {appName}</h4>
-										
-										<div className="mb-3">
-											<label htmlFor="login-username" className="form-label">Kullanıcı Adı</label>
-											<div className="input-group">
-												<i className="fa-sharp fa-thin fa-at"></i>
-												<input type="text" className="form-control" id="login-username"
-													value={username}
-													onChange={(e) => setUsername(e.target.value)}
-												/>
-											</div>
+										<div className="form-check">
+											<input type="checkbox" className="form-check-input" id="login-remember"
+												checked={remember}
+												onChange={(e) => setRemember(e.target.checked)}
+											/>
+											<label htmlFor="login-remember" className="form-check-label">Beni Hatırla</label>
 										</div>
 										
-										<div className="mb-3">
-											<label htmlFor="login-password" className="form-label">Şifre</label>
-											<div className="input-group">
-												<i className="fa-sharp fa-thin fa-lock"></i>
-												<input type="password" className="form-control" id="login-password" placeholder="******"
-													value={password}
-													onChange={(e) => setPassword(e.target.value)}
-												/>
-											</div>
-										</div>
-										
-										<div className="d-flex justify-content-between mb-3">
-											
-											<div className="form-check">
-												<input type="checkbox" className="form-check-input" id="login-remember"
-													checked={remember}
-													onChange={(e) => setRemember(e.target.checked)}
-												/>
-												<label htmlFor="login-remember" className="form-check-label">Beni Hatırla</label>
-											</div>
-											
-											<div className="">
-												<Link to="/auth/forgot-password">Şifremi Unuttum</Link>
-											</div>
-										
-										
-										</div>
-										
-										<div className="d-grid mb-3">
-											<button type="button" className="btn btn-theme" onClick={loginHandler}>
-												<span>Continue</span>
-												<i className="fa-sharp fa-thin fa-angle-double-right"></i>
-											</button>
+										<div className="">
+											<Link to="/auth/forgot-password">Şifremi Unuttum</Link>
 										</div>
 									
+									
+									</div>
+									
+									<div className="d-grid mb-3">
+										<button type="button" className="btn btn-theme" onClick={loginHandler}>
+											<span>Continue</span>
+											<i className="fa-sharp fa-thin fa-angle-double-right"></i>
+										</button>
 									</div>
 								
 								</div>
 							
 							</div>
+						
 						</div>
 					</div>
 				</div>
 			</div>
 		</div>
-	);
+	</div>);
 };
 
 export default Login;
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,4 +1,3 @@
-// src/pages/Register.jsx
 import React from 'react';
 
 const Register = () => {
@@ -6,15 +5,9 @@ const Register = () => {
 		<div>
 			<h1>Kayıt Sayfası</h1>
 			<form>
-				<label>Ad:</label>
-				<input type="text" placeholder="Adınız" />
-				<br />
-				<label>Email:</label>
-				<input type="email" placeholder="Email" />
-				<br />
-				<label>Şifre:</label>
-				<input type="password" placeholder="Şifre" />
-				<br />
+				<label>Ad:</label> <input type="text" placeholder="Adınız"/> <br/> <label>Email:</label>
+				<input type="email" placeholder="Email"/> <br/> <label>Şifre:</label>
+				<input type="password" placeholder="Şifre"/> <br/>
 				<button type="submit">Kayıt Ol</button>
 			</form>
 		</div>
